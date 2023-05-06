1b1941180133d346550df1b495902581ad9eeb12
CorpoSeco <122288216+CorpoSeco@users.noreply.github.com>
2023-05-06 10:17:01 -0300

    Add files via upload

---
+++ ROtoRW.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pkg install android-tools
+pkg install e2fsprogs
+
+set -e
+
+if [ -f "$1" ]; then
+   srcFile="$1"
+fi
+
+if [ ! -f "$srcFile" ];then
+	echo "Usage: bash ROtoRW.sh [/path/to/system.img]"
+	exit 1
+fi
+
+simg2img "$srcFile" system.img || mv "$srcFile" system.img
+
+e2fsck -y -f system.img
+resize2fs system.img 4000M
+e2fsck -E unshare_blocks -y -f system.img
+e2fsck -f -y system.img
+resize2fs -M system.img
